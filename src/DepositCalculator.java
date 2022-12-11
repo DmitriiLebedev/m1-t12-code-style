eb0a40b32bbbb9d6569477db10ab76391d49195a
10FPS <dmtr990@gmail.com>
2022-12-11 22:13:42 +0300

    переименован медод взаимодействия с юзером

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -16,7 +16,7 @@ public class DepositCalculator {
         return calculateDeposit(amount + amount * yearRate * depositPeriod);
     }
 
-    double calculateDeposit(double payout) { //не совсем понял зачем была изменена сигнатура метода
+    double calculateDeposit(double payout) {
         double scale = Math.pow(10, 2); 
         return Math.round(payout * scale) / scale;
     }
@@ -24,7 +24,7 @@ public class DepositCalculator {
     void chooseDeposit() {
         int amount;
         int depositPeriod;
-        int input;
+        int chooseTypeOfDeposit;
         double payout = 0;
         double yearRate = 0.06;
 
@@ -33,9 +33,9 @@ public class DepositCalculator {
         System.out.println("Введите срок вклада в годах:");
         depositPeriod = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
-        input = scanner.nextInt(); //я бы эту переменную назвал более звучно, чтоб название отражало для чего она.
-        if (input == 1) payout = calculateSimplePercent(amount, yearRate, depositPeriod);
-        else if (input == 2) {
+        chooseTypeOfDeposit = scanner.nextInt();
+        if (chooseTypeOfDeposit == 1) payout = calculateSimplePercent(amount, yearRate, depositPeriod);
+        else if (chooseTypeOfDeposit == 2) {
             payout = calculateComplexPercent(amount, yearRate, depositPeriod);
         }
         System.out.println("Результат вклада: " + amount + " за " + depositPeriod + " лет превратятся в " + payout);
